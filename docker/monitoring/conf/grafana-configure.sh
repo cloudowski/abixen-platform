5eb4f15865aaf629531341506b1eb1c827f53c3d
Tomasz Cholewa <tomasz@cloudowski.com>
2017-03-09 15:08:26 +0100

    New dash for db

--- docker/monitoring/conf/grafana-configure.sh
+++ docker/monitoring/conf/grafana-configure.sh
@@ -2,4 +2,4 @@ sleep 20
 
 curl -v  http://grafana:3000/api/datasources -d @/conf/datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 curl -v  http://grafana:3000/api/dashboards/db -d @/conf/dash.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
-
+curl -v  http://grafana:3000/api/dashboards/db -d @/conf/dash-db.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
